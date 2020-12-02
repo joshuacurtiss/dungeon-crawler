719b00ba8303b613bbb8977dd4be75b2d2d8cdd9
Josh Curtiss <J.Curtiss@rockvalleycollege.edu>
2020-12-02 00:18:26 -0600

    Refactor enemy to have a parent Enemy class. This is setup to allow multiple enemies which will all inherit the Enemy class and then have specific behaviors in each of their individual classes.

---
+++ src/enemies/Enemy.ts
@@ -0,0 +1,58 @@
+import Phaser from 'phaser'
+
+export default class Enemy extends Phaser.Physics.Arcade.Sprite {
+
+    public damageInflicted: number = 1
+    public speed: number = 50
+    protected animIdle?: string
+    protected animRun?: string
+    private _direction!: Phaser.Math.Vector2
+    protected moveEvent?: Phaser.Time.TimerEvent
+
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string|number) {
+        super(scene, x, y, texture, frame)
+        this.direction = new Phaser.Math.Vector2(0, 0)
+        scene.physics.world.on(Phaser.Physics.Arcade.Events.TILE_COLLIDE, this.handleTileCollision, this)
+    }
+
+    get direction(): Phaser.Math.Vector2 {
+        return this._direction
+    }
+
+    set direction(vec: Phaser.Math.Vector2) {
+        if( !vec.x && !vec.y && this.animIdle ) this.anims.play(this.animIdle)
+        else if( (vec.x || vec.y) && this.animRun ) this.anims.play(this.animRun)
+        this._direction = vec
+    }
+
+    destroy(fromScene?: boolean) {
+        this.moveEvent?.destroy()
+        super.destroy(fromScene)
+    }
+
+    protected changeDirection() {
+        this.direction = this.randomDirection()
+    }
+
+    protected randomDirection(): Phaser.Math.Vector2 {
+        return new Phaser.Math.Vector2(Phaser.Math.Between(-1,1) * this.speed, Phaser.Math.Between(-1,1) * this.speed)
+    }
+    
+    private handleTileCollision(go: Phaser.GameObjects.GameObject) {
+        if( go!==this ) return
+        this.direction = this.randomDirection()
+    }
+
+    preUpdate(t: number, dt: number) {
+        super.preUpdate(t, dt)
+        this.setVelocity(this.direction.x, this.direction.y)
+        if( this.direction.x<0 ) {
+            this.scaleX = -1
+            this.body.offset.x = 16
+        } else if( this.direction.x>0 ) {
+            this.scaleX = 1
+            this.body.offset.x = 0
+        }
+    }
+
+}
--- src/enemies/Lizard.ts
+++ src/enemies/Lizard.ts
@@ -1,56 +1,27 @@
 import Phaser from 'phaser'
+import Enemy from './Enemy'
 
-export default class Lizard extends Phaser.Physics.Arcade.Sprite {
+export default class Lizard extends Enemy {
 
-    public damageInflicted = 0.5
-    private speed = Phaser.Math.Between(40, 75)
-    private direction = new Phaser.Math.Vector2(this.speed, 0)
-    private moveEvent: Phaser.Time.TimerEvent
+    public damageInflicted: number = 0.5
+    public speed: number = Phaser.Math.Between(40, 75)
+
+    protected animIdle: string = 'lizard-idle'
+    protected animRun: string = 'lizard-run'
 
     constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame?: string|number) {
         super(scene, x, y, texture, frame)
-        this.anims.play('lizard-run')
-        scene.physics.world.on(Phaser.Physics.Arcade.Events.TILE_COLLIDE, this.handleTileCollision, this)
+        this.direction = this.randomDirection()
         this.moveEvent = scene.time.addEvent({
             delay: Phaser.Math.Between(1500, 5000),
-            callback: ()=>{
-                this.direction = this.randomDirection()
-            },
+            callback: ()=>this.changeDirection(),
             loop: true
         })
     }
 
     destroy(fromScene?: boolean) {
 		this.scene.sound.play('monster-' + Phaser.Math.Between(1,5))
-        this.moveEvent.destroy()
         super.destroy(fromScene)
-    }
-
-    private randomDirection(): Phaser.Math.Vector2 {
-        let newDir: Phaser.Math.Vector2
-        do {
-            newDir = new Phaser.Math.Vector2(Phaser.Math.Between(-1,1) * this.speed, Phaser.Math.Between(-1,1) * this.speed)
-        } while ((this.direction && newDir.fuzzyEquals(this.direction)) || (newDir.x===0 && newDir.y===0))
-        return newDir
-    }
-    
-
-    private handleTileCollision(go: Phaser.GameObjects.GameObject, tile: Phaser.Tilemaps.Tile) {
-        if( go!==this ) return
-        this.direction = this.randomDirection()
-    }
-
-    preUpdate( t: number, dt: number) {
-        super.preUpdate(t, dt)
-        this.setVelocity(this.direction.x, this.direction.y)
-        if( this.direction.y!==0 ) return
-        if( this.direction.x<0 ) {
-            this.scaleX = -1
-            this.body.offset.x = 16
-        } else if( this.direction.x>0 ) {
-            this.scaleX = 1
-            this.body.offset.x = 0
-        }
     }
 
 }
