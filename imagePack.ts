7e1cab678adca649b62cd45b0025f3c0df76cd98
Joshua Curtiss <j.curtiss@rockvalleycollege.edu>
2021-02-02 12:38:57 -0600

    Add all images, and image packing script that combines into one texture file.
    This is all animations or static images for all sprites. The tilesets are still separate.
    
    Please note the images have been slightly renamed from their original names when packed separately. This means the code will need to be tweaked for the new names before the packed texture file can be used. So right now all of these files and the packing script are extra and not used. The renaming does make the names more concise and more consistent.

--- imagePack.ts
+++ imagePack.ts
@@ -1,22 +1,46 @@
 import * as fs from 'fs'
-import { packAsync } from 'free-tex-packer-core'
- 
-const images = [
-    {path: "weapon_fireball.png", contents: fs.readFileSync("./public/items/weapon_fireball.png")},
-    {path: "weapon_knife.png", contents: fs.readFileSync("./public/items/weapon_knife.png")},
-    {path: "weapon_regular_sword.png", contents: fs.readFileSync("./public/items/weapon_regular_sword.png")},
-]
- 
-async function packImages() {
+import * as path from 'path'
+import {packAsync} from 'free-tex-packer-core'
+
+interface Img {
+    path: string;
+    contents: Buffer;
+}
+
+function findImages(dir: string): string[] {
+    const images: string[] = []
+    fs.readdirSync(dir).forEach(filename=>{
+        const fullPath = path.resolve(dir, filename)
+        const stat = fs.statSync(fullPath)
+        if( stat && stat.isDirectory() ) images.push(...findImages(fullPath))
+        else if( stat && stat.isFile() && path.extname(filename).toLowerCase()==='.png' ) images.push(fullPath)
+    })
+    return images
+}
+
+async function packImages(images: Img[]) {
     try {
-        const files = await packAsync(images)
-        files.forEach(item=>{
-            console.log(item.name)
-            fs.writeFileSync('./public/' + item.name, item.buffer)
+        const files = await packAsync(images, {
+            textureName: 'textures',
+            padding: 1,
+            allowRotation: true,
+            detectIdentical: true,
+            allowTrim: true,
+            removeFileExtension: true,
         })
+        files.forEach(item=>fs.writeFileSync(path.resolve(__dirname, 'dist', item.name), item.buffer))
     } catch(error) {
         console.log(error)
     }
 }
 
-packImages()
+const images: Img[] = findImages(path.resolve(__dirname, 'resources', 'images')).map(fullPath=>{
+    return {
+        path: path.basename(fullPath),
+        contents: fs.readFileSync(fullPath),
+    }
+})
+
+const dest = path.resolve(__dirname, 'dist')
+if( !fs.existsSync(dest) ) fs.mkdirSync(dest)
+packImages(images)
